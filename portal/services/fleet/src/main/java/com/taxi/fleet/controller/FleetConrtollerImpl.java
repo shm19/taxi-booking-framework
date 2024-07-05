77142124fd0fc9c8d9e784234afd2f132bb17742
shm <malekishayan79@gmail.com>
2024-07-05 16:45:58 +0330

    feat(FleetControllerImpl): add log logic

--- portal/services/fleet/src/main/java/com/taxi/fleet/controller/FleetConrtollerImpl.java
+++ portal/services/fleet/src/main/java/com/taxi/fleet/controller/FleetConrtollerImpl.java
@@ -4,19 +4,38 @@ import com.taxi.fleet.service.FleetServiceImpl;
 import com.taxi.framework.fleet.controller.AbstractFleetController;
 import com.taxi.framework.fleet.dto.BaseFleetDriverDTO;
 import com.taxi.framework.fleet.dto.BaseFleetUserDTO;
-import com.taxi.framework.fleet.service.FleetService;
+import com.taxi.user.logging.UserActionLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/fleet")
 @RestController
-public class FleetConrtollerImpl extends AbstractFleetController<BaseFleetUserDTO, BaseFleetDriverDTO> {
+public class FleetControllerImpl extends AbstractFleetController<BaseFleetUserDTO, BaseFleetDriverDTO> {
+
+    @Autowired
+    private UserActionLogger userActionLogger;
 
     @Autowired
     private FleetServiceImpl fleetService;
 
-    protected FleetConrtollerImpl(FleetServiceImpl fleetService) {
+    protected FleetControllerImpl(FleetServiceImpl fleetService, UserActionLogger userActionLogger) {
         super(fleetService);
+        this.userActionLogger = userActionLogger;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerVehicle(@RequestBody BaseFleetUserDTO vehicleDTO) {
+        userActionLogger.logUserAction(vehicleDTO.getUsername(), "register vehicle");
+        fleetService.registerVehicle(vehicleDTO);
+        return ResponseEntity.ok("Vehicle registered");
+    }
+
+    @PutMapping("/update-status")
+    public ResponseEntity<?> updateVehicleStatus(@RequestBody BaseFleetDriverDTO vehicleDTO, @RequestParam String status) {
+        userActionLogger.logUserAction(vehicleDTO.getUsername(), "update vehicle status to " + status);
+        fleetService.updateVehicleStatus(vehicleDTO, status);
+        return ResponseEntity.ok("Vehicle status updated");
     }
 }
