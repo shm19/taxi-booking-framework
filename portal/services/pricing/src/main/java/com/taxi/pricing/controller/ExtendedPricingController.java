78b7e84b39e5c37e99d299fd80e5565dc375b683
shm <malekishayan79@gmail.com>
2024-07-05 16:43:47 +0330

    feat(ExtendedPricingController): add log logic

--- portal/services/pricing/src/main/java/com/taxi/pricing/controller/ExtendedPricingController.java
+++ portal/services/pricing/src/main/java/com/taxi/pricing/controller/ExtendedPricingController.java
@@ -4,14 +4,38 @@ import com.taxi.framework.pricing.controller.AbstractPricingController;
 import com.taxi.framework.pricing.service.AbstractPricingServiceImpl;
 import com.taxi.pricing.dto.ExtendedBasePricingDto;
 import com.taxi.pricing.dto.ExtendedBaseResponsePricingDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.taxi.user.logging.UserActionLogger;
+import com.taxi.pricing.service.ExtendedPricingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/pricing")
 @RestController
 public class ExtendedPricingController extends AbstractPricingController<ExtendedBasePricingDto, ExtendedBaseResponsePricingDto> {
 
-    protected ExtendedPricingController(AbstractPricingServiceImpl<ExtendedBasePricingDto, ExtendedBaseResponsePricingDto> pricingService) {
+    @Autowired
+    private UserActionLogger userActionLogger;
+
+    @Autowired
+    private ExtendedPricingService pricingService;
+
+    protected ExtendedPricingController(AbstractPricingServiceImpl<ExtendedBasePricingDto, ExtendedBaseResponsePricingDto> pricingService, UserActionLogger userActionLogger) {
         super(pricingService);
+        this.userActionLogger = userActionLogger;
+    }
+
+    @PostMapping("/calculate")
+    public ResponseEntity<?> calculatePrice(@RequestBody ExtendedBasePricingDto pricingDto) {
+        userActionLogger.logUserAction(pricingDto.getUserId(), "calculate price");
+        ExtendedBaseResponsePricingDto response = pricingService.getResponse(pricingDto);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updatePricing(@RequestBody ExtendedBasePricingDto pricingDto) {
+        userActionLogger.logUserAction(pricingDto.getUserId(), "update pricing");
+        pricingService.updatePricing(pricingDto);
+        return ResponseEntity.ok("Pricing updated");
     }
 }
