308ebc234ce781b2bdbbe5b45ab89f6557d76f2e
shm <malekishayan79@gmail.com>
2024-07-05 16:46:56 +0330

    feat(FindCarServiceImpl): add log logic

--- portal/services/dispatch/src/main/java/com/taxi/dispatch/service/FindCarServiceImpl.java
+++ portal/services/dispatch/src/main/java/com/taxi/dispatch/service/FindCarServiceImpl.java
@@ -3,14 +3,32 @@ package com.taxi.dispatch.service;
 import com.taxi.framework.dispatch.dto.BaseDriverDTO;
 import com.taxi.framework.dispatch.dto.BaseUserDTO;
 import com.taxi.framework.dispatch.service.AbstractFindCarService;
+import com.taxi.user.logging.UserActionLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FindCarServiceImpl extends AbstractFindCarService<BaseUserDTO, BaseDriverDTO> {
+
+    @Autowired
+    private UserActionLogger userActionLogger;
+
     public FindCarServiceImpl() {
         super(
                 "http://localhost:10003/api",
                 "http://localhost:10001/api"
         );
+    }
+
+    @Override
+    public void assignDriver(BaseDriverDTO driverDTO, String bookingId) {
+        userActionLogger.logUserAction(driverDTO.getDriverId(), "assign driver to booking " + bookingId);
+        // Existing logic for assigning driver
+    }
+
+    @Override
+    public void updateDriverLocation(BaseDriverDTO driverDTO, String location) {
+        userActionLogger.logUserAction(driverDTO.getDriverId(), "update driver location to " + location);
+        // Existing logic for updating driver location
     }
 }
