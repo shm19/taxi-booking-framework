9e25c8ef2bde2b0c8b774840c6827c14c80de8e7
shm <malekishayan79@gmail.com>
2024-07-05 16:50:01 +0330

    fix(BookingApplication): scan packages

--- portal/services/booking/src/main/java/com/taxi/booking/BookingApplication.java
+++ portal/services/booking/src/main/java/com/taxi/booking/BookingApplication.java
@@ -3,11 +3,10 @@ package com.taxi.booking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.taxi.booking", "com.taxi.user.logging"})
 public class BookingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BookingApplication.class, args);
+    }
 }
