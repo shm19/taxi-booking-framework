29c6c4a96bc12d1565b8ef99f5df40d4f5acbc62
shm <malekishayan79@gmail.com>
2024-07-05 16:41:57 +0330

    feat(AuthController): add log logic

--- portal/services/user/src/main/java/com/taxi/user/controller/AuthControllerImplement.java
+++ portal/services/user/src/main/java/com/taxi/user/controller/AuthControllerImplement.java
@@ -2,19 +2,34 @@ package com.taxi.user.controller;
 
 import com.taxi.user.dto.ExtendedUserSigninDTO;
 import com.taxi.framework.user.controller.AbstractAuthController;
+import com.taxi.user.logging.UserActionLogger;
 import com.taxi.user.service.ExtendedAuthServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequestMapping("/api/user")
 @RestController
 public class AuthControllerImplement extends AbstractAuthController<ExtendedUserSigninDTO> {
 
     @Autowired
+    private UserActionLogger userActionLogger;
+
+    @Autowired
     private ExtendedAuthServiceImpl extendedAuthService;
 
-    public AuthControllerImplement(ExtendedAuthServiceImpl extendedAuthService) {
+    public AuthControllerImplement(ExtendedAuthServiceImpl extendedAuthService, UserActionLogger userActionLogger) {
         super(extendedAuthService);
+        this.userActionLogger = userActionLogger;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody UserLoginDTO userLoginDTO) {
+        userActionLogger.logUserAction(userLoginDTO.getUsername(), "login attempt");
+        // Perform login logic
+        return ResponseEntity.ok("User logged in");
     }
 }
