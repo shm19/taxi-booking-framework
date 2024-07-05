1c9c602a0a7c7ca16a86ace83f655e19de733f6a
shm <malekishayan79@gmail.com>
2024-07-05 16:48:14 +0330

    fix(dispachApp): scanBasePackages

--- portal/services/dispatch/src/main/java/com/taxi/dispatch/DispatchApplication.java
+++ portal/services/dispatch/src/main/java/com/taxi/dispatch/DispatchApplication.java
@@ -3,11 +3,10 @@ package com.taxi.dispatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.taxi.dispatch", "com.taxi.user.logging"})
 public class DispatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DispatchApplication.class, args);
     }
-
 }
