d5e35ab1d875f4f0a9fcf2b75448b8032b959f58
shm <malekishayan79@gmail.com>
2024-07-05 16:40:43 +0330

    feat(ExtendedAuthServiceImpl): add log logic

--- portal/services/user/src/main/java/com/taxi/user/service/ExtendedAuthServiceImpl.java
+++ portal/services/user/src/main/java/com/taxi/user/service/ExtendedAuthServiceImpl.java
@@ -1,14 +1,22 @@
 package com.taxi.user.service;
 
 import com.taxi.user.dto.ExtendedUserSigninDTO;
+import com.taxi.user.logging.UserActionLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 @Qualifier("AuthService")
 public class ExtendedAuthServiceImpl implements ExtendedAuthService {
+
+    @Autowired
+    private UserActionLogger userActionLogger;
+
     @Override
     public ExtendedUserSigninDTO signIn(ExtendedUserSigninDTO dto) {
+        userActionLogger.logUserAction(dto.getUsername(), "authentication attempt");
+        // Preserving existing logic
         return dto;
     }
 }
