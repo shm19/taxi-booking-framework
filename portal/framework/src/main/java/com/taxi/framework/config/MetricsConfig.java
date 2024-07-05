fad4f2f72fbb3b7a1bf996fe365e89cef1b57de0
shm <malekishayan79@gmail.com>
2024-07-05 16:50:30 +0330

    feat(MetricsConfig): add performance analys

---
+++ portal/framework/src/main/java/com/taxi/framework/config/MetricsConfig.java
@@ -0,0 +1,50 @@
+package com.taxi.framework.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.jvm.GarbageCollectorMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "TaxiBookingService");
+    }
+
+    @Bean
+    public ClassLoaderMetrics classLoaderMetrics() {
+        return new ClassLoaderMetrics();
+    }
+
+    @Bean
+    public JvmMemoryMetrics jvmMemoryMetrics() {
+        return new JvmMemoryMetrics();
+    }
+
+    @Bean
+    public JvmThreadMetrics jvmThreadMetrics() {
+        return new JvmThreadMetrics();
+    }
+
+    @Bean
+    public GarbageCollectorMetrics garbageCollectorMetrics() {
+        return new GarbageCollectorMetrics();
+    }
+
+    @Bean
+    public ProcessorMetrics processorMetrics() {
+        return new ProcessorMetrics();
+    }
+
+    @Bean
+    public UptimeMetrics uptimeMetrics() {
+        return new UptimeMetrics();
+    }
+}
