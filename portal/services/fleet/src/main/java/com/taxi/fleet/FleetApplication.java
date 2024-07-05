dc4df9ba7a86751afa1623132c503d7ac3107cf6
shm <malekishayan79@gmail.com>
2024-07-05 16:46:13 +0330

    fix(fleetApp): scan package

--- portal/services/fleet/src/main/java/com/taxi/fleet/FleetApplication.java
+++ portal/services/fleet/src/main/java/com/taxi/fleet/FleetApplication.java
@@ -3,11 +3,10 @@ package com.taxi.fleet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.taxi.fleet", "com.taxi.user.logging"})
 public class FleetApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FleetApplication.class, args);
     }
-
 }
