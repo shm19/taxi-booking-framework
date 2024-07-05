455be7fda9ee9c0a4d306d10836096fc5b09f6db
shm <malekishayan79@gmail.com>
2024-07-05 16:39:44 +0330

    feat(signup): add logic

--- portal/services/user/src/main/java/com/taxi/user/service/SignupServiceImpl.java
+++ portal/services/user/src/main/java/com/taxi/user/service/SignupServiceImpl.java
@@ -2,14 +2,21 @@ package com.taxi.user.service;
 
 import com.taxi.framework.user.dto.BaseUserSignupDTO;
 import com.taxi.framework.user.service.SignupService;
+import com.taxi.user.logging.UserActionLogger;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Qualifier("SignupService")
 public class SignupServiceImpl implements SignupService<BaseUserSignupDTO> {
+
+    @Autowired
+    private UserActionLogger userActionLogger;
+
     @Override
     public BaseUserSignupDTO signUp(BaseUserSignupDTO dto) {
+        userActionLogger.logUserAction(dto.getUsername(), "signup");
         return dto;
     }
 }
